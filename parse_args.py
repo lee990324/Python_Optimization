0e2f9a4253a51e2408fc5dc947b3597e5e876679
mintmelon <f9977f990324@gmail.com>
2024-09-25 10:43:49 +0900

    modified: parse_args

--- parse_args.py
+++ parse_args.py
@@ -6,145 +6,36 @@ from dataset import DATASET_NAMES, dataset_info
 IS_LINUX = True if platform.system()=="Linux" else False
 
 def parse_args():
-    """Parse command line arguments."""
-    parser = argparse.ArgumentParser(description='LDC trainer.')
-    parser.add_argument('--choose_test_data',
+    parser = argparse.ArgumentParser(description='프로그램 설명')
+    parser.add_argument('--choose_data',
                         type=int,
-                        default=-1, # uded 14
-                        help='Choose a dataset for testing: 0 - 8')
-    # ----------- test -------0--
+                        default=-1, # test
+                        help='Choose a dataset for testing: 0 - 1')
 
-
-    TEST_DATA = DATASET_NAMES[parser.parse_args().choose_test_data] # max 8
-    test_inf = dataset_info(TEST_DATA, is_linux=IS_LINUX)
-    test_dir = test_inf['data_dir']
-    is_testing =False
-
-    # Training settings
-    # BIPED-B2=1, BIPDE-B3=2, just for evaluation, using LDC trained with 2 or 3 bloacks
-    TRAIN_DATA = DATASET_NAMES[0] # BIPED=0, BRIND=6, MDBD=10
-    train_inf = dataset_info(TRAIN_DATA, is_linux=IS_LINUX)
-    train_dir = train_inf['data_dir']
+    dataset_names = DATASET_NAMES[parser.parse_args().choose_data]
+    data_inf = dataset_info(dataset_names, is_linux=IS_LINUX)
+    option1 = data_inf['option1']
+    option2 = data_inf['option2']
+    option3 = data_inf['option3']
+    is_testing = False
 
     # Data parameters
     parser.add_argument('--input_dir',
                         type=str,
-                        default=train_dir,
+                        default=option3,
                         help='the path to the directory with the input data.')
-    parser.add_argument('--input_val_dir',
-                        type=str,
-                        default=test_inf['data_dir'],
-                        help='the path to the directory with the input data for validation.')
     parser.add_argument('--output_dir',
                         type=str,
-                        default='checkpoints',
+                        default='ouput',
                         help='the path to output the results.')
-    parser.add_argument('--train_data',
+    parser.add_argument('--data_name',
                         type=str,
                         choices=DATASET_NAMES,
-                        default=TRAIN_DATA,
-                        help='Name of the dataset.')# TRAIN_DATA,BIPED-B3
-    parser.add_argument('--test_data',
-                        type=str,
-                        choices=DATASET_NAMES,
-                        default=TEST_DATA,
+                        default=dataset_names,
                         help='Name of the dataset.')
-    parser.add_argument('--test_list',
-                        type=str,
-                        default=test_inf['test_list'],
-                        help='Dataset sample indices list.')
-    parser.add_argument('--train_list',
-                        type=str,
-                        default=train_inf['train_list'],
-                        help='Dataset sample indices list.')
     parser.add_argument('--is_testing',type=bool,
                         default=is_testing,
                         help='Script in testing mode.')
-    parser.add_argument('--predict_all',
-                        type=bool,
-                        default=False,
-                        help='True: Generate all LDC outputs in all_edges ')
-    parser.add_argument('--double_img',
-                        type=bool,
-                        default=False,
-                        help='True: use same 2 imgs changing channels')  # Just for test
-    parser.add_argument('--resume',
-                        type=bool,
-                        default=False,
-                        help='use previous trained data')  # Just for test
-    parser.add_argument('--checkpoint_data',
-                        type=str,
-                        default='16/16_model.pth',# 37 for biped 60 MDBD
-                        help='Checkpoint path.')
-    parser.add_argument('--test_img_width',
-                        type=int,
-                        default=test_inf['img_width'],
-                        help='Image width for testing.')
-    parser.add_argument('--test_img_height',
-                        type=int,
-                        default=test_inf['img_height'],
-                        help='Image height for testing.')
-    parser.add_argument('--res_dir',
-                        type=str,
-                        default='result',
-                        help='Result directory')
-    parser.add_argument('--log_interval_vis',
-                        type=int,
-                        default=100,
-                        help='The NO B to wait before printing test predictions. 200')
 
-    parser.add_argument('--epochs',
-                        type=int,
-                        default=25,
-                        metavar='N',
-                        help='Number of training epochs (default: 25).')
-    parser.add_argument('--lr', default=5e-5, type=float,
-                        help='Initial learning rate. =5e-5')
-    parser.add_argument('--lrs', default=[25e-4,5e-4,1e-5], type=float,
-                        help='LR for set epochs')
-    parser.add_argument('--wd', type=float, default=0., metavar='WD',
-                        help='weight decay (Good 5e-6)')
-    parser.add_argument('--adjust_lr', default=[6,12,18], type=int,
-                        help='Learning rate step size.')  # [6,9,19]
-    parser.add_argument('--version_notes',
-                        default='LDC-BIPED: B4 Exp 67L3 xavier init normal+ init normal CatsLoss2 Cofusion',
-                        type=str,
-                        help='version notes')
-    parser.add_argument('--batch_size',
-                        type=int,
-                        default=8,
-                        metavar='B',
-                        help='the mini-batch size (default: 8)')
-    parser.add_argument('--workers',
-                        default=8,
-                        type=int,
-                        help='The number of workers for the dataloaders.')
-    parser.add_argument('--tensorboard',type=bool,
-                        default=True,
-                        help='Use Tensorboard for logging.'),
-    parser.add_argument('--img_width',
-                        type=int,
-                        default=352,
-                        help='Image width for training.') # BIPED 352 BSDS 352/320 MDBD 480
-    parser.add_argument('--img_height',
-                        type=int,
-                        default=352,
-                        help='Image height for training.') # BIPED 480 BSDS 352/320
-    parser.add_argument('--channel_swap',
-                        default=[2, 1, 0],
-                        type=int)
-    parser.add_argument('--resume_chpt',
-                        default='result/resume/',
-                        type=str,
-                        help='resume training')
-    parser.add_argument('--crop_img',
-                        default=True,
-                        type=bool,
-                        help='If true crop training images, else resize images to match image width and height.')
-    parser.add_argument('--mean_pixel_values',
-                        default=[103.939,116.779,123.68,137.86],
-                        type=float)  # [103.939,116.779,123.68,137.86] [104.00699, 116.66877, 122.67892]
-    # BRIND mean = [104.007, 116.669, 122.679, 137.86]
-    # BIPED mean_bgr processed [160.913,160.275,162.239,137.86]
     args = parser.parse_args()
     return args
