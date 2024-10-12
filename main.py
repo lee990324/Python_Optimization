e1aa58d61aadb677fb6c6d939099e37a2beb52e6
mintWaterMelon <134702226+lee990324@users.noreply.github.com>
2024-10-12 19:23:23 +0900

    add: main

---
+++ main.py
@@ -0,0 +1,11 @@
+from parse_args import parse_args
+
+def main(args):
+    print('-------------------------------------------------------')
+    print('LDC parameters:')
+    print('-------------------------------------------------------')
+
+
+if __name__ == '__main__':
+    args = parse_args()
+    main(args)
