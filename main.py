5d2b182ce736d79079100d3c3acd1b907c205d37
mintWaterMelon <134702226+lee990324@users.noreply.github.com>
2024-10-13 17:02:50 +0900

    modified

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ from parse_args import parse_args
 
 def main(args):
     print('-------------------------------------------------------')
-    print('LDC parameters:')
+    print('dataset_names:', args.data_name)
+    print('Option2:', args.option2)
+    print('Input1:', args.input1)
     print('-------------------------------------------------------')
 
 
